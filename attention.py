808d7c240274449398928fdaae67e6db14e2c5db
umairgillani93 <umairgillani93@gmail.com>
2025-08-28 15:24:57 +0500

    query, key and values for attention to compute context vector

--- attention.py
+++ attention.py
@@ -16,3 +16,37 @@ all_context_vectors = inputs.T @ att_weights
 
 print(all_context_vectors)
 
+torch.manual_seed(123)
+
+# dimentions in and out for query, key and value vectors
+d_in = 2
+d_out = 3
+
+# inititlize the query, kye and value weights vectors
+w_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad = False)
+w_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad = False)
+w_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad = False)
+
+# some randome input it may be a word and having some vector-embeddings
+# of size 3 * 1
+x = torch.randn(3, 1)
+
+    
+# now using weights and inputs we'll calculate query, key and value vectors
+key_vector= w_key @ x
+query_vector= w_query @ x
+value_vector= w_value @ x
+
+
+# the un-scaled attention score calculations
+                 # word
+            # x(x) -> [0.1, 1.3, -0.2]
+            #   |                   \
+            # wk -> [0.2, 2.3, 2.2]  wv -> [2.3, 3.1, 4.1]
+            #   |                           |
+            # wq -> [0.4, 0.2]              |
+            #   |                           |
+            #   \                          /
+            #          context vector [0.2, 0.3, -0.1]
+
+
